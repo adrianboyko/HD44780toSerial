bc60169e834db6a6b5b5e4dfbbec997fabb78c6b
Adrian <adrianboyko@gmail.com>
2020-11-24 20:56:19 -0700

    Added namespaces, no longer malloc buffer,  detach LCD EN interrupt on uSDX power down.

--- HD44780toSerial.ino
+++ HD44780toSerial.ino
@@ -1,31 +1,34 @@
 
-#include <avr/io.h>
-
 const int BUFFER_SIZE = 1024;
 const int BUFFER_INDEX_MASK = BUFFER_SIZE - 1;
 
-const int ROT_B_PIN          =  8;
-const int ROT_A_PIN          =  9;
-const int RIGHT_BUTTON_PIN   = 10;
-const int LEFT_BUTTON_PIN    = 11;
-const int ENCODER_BUTTON_PIN = 12;
-const int LCD_ENABLE_PIN     =  2; // This is used for EN clocking, i.e. to catch uSDX sending cmd/data to LCD.
-const int LCD_POWER_PIN      =  6; // This will be used to detect uSDX power up and power down.
-const int PUSH_TO_TALK_PIN   =  7; 
+namespace Pin {
+  const int ROT_B          =  8;
+  const int ROT_A          =  9;
+  const int RIGHT_BUTTON   = 10;
+  const int LEFT_BUTTON    = 11;
+  const int ENCODER_BUTTON = 12;
+  const int LCD_ENABLE     =  2; // This is used for EN clocking, i.e. to catch uSDX sending cmd/data to LCD.
+  const int LCD_POWER      =  6; // This will be used to detect uSDX power up and power down.
+  const int PUSH_TO_TALK   =  7; 
+  const int USDX_RESET     =  5; // This is connected to the uSDX's reset pin.
+}
 
-const unsigned long BUTTON_CLICK_DURATION = 5;  // milliseconds
+const unsigned long PULSE_DURATION = 5;  // milliseconds
 
-const byte CLICK_LEFT_BUTTON               = 1;
-const byte CLICK_RIGHT_BUTTON              = 2;
-const byte CLICK_ENCODER_BUTTON            = 3;
-const byte ROTATE_ENCODER_CLOCKWISE        = 4;
-const byte ROTATE_ENCODER_COUNTERCLOCKWISE = 5;
-const byte PUSH_TO_TALK_START              = 6;
-const byte PUSH_TO_TALK_END                = 7;
+enum gesture_t {
+  CLICK_LEFT_BUTTON               = 1,
+  CLICK_RIGHT_BUTTON              = 2,
+  CLICK_ENCODER_BUTTON            = 3,
+  ROTATE_ENCODER_CLOCKWISE        = 4,
+  ROTATE_ENCODER_COUNTERCLOCKWISE = 5,
+  PUSH_TO_TALK_START              = 6,
+  PUSH_TO_TALK_END                = 7,
+  RESET_THE_USDX                  = 8
+};
 
 volatile bool usdxPowerOn = false;
-
-volatile unsigned char *volatile lcdBuffer = malloc(BUFFER_SIZE);
+volatile unsigned char lcdBuffer[BUFFER_SIZE];
 volatile int idxOfNextLcdWrite = 0;
 volatile int idxOfNextLcdRead = 0;
 
@@ -47,126 +50,162 @@ void lcdActivityISR() {
   }
 }
 
+namespace Button {
+
+  unsigned long timeOfClickStart = 0;
+  int pinOfButtonClicked = 0;
+
+  bool clickInProgress() {
+    return pinOfButtonClicked > 0;
+  }
+
+  void startClick(int buttonPin, bool pushedState) {
+    pinMode(buttonPin, OUTPUT);
+    digitalWrite(buttonPin, pushedState);
+    timeOfClickStart = millis();
+    pinOfButtonClicked = buttonPin;
+  }
+
+  void endClick() {
+    bool newState = !digitalRead(pinOfButtonClicked);
+    digitalWrite(pinOfButtonClicked, newState);
+    pinMode(pinOfButtonClicked, INPUT);  // No PULLDOWN because there's already a pulldown on the uSDX side.
+    timeOfClickStart = 0; // meaning "none"
+    pinOfButtonClicked = 0; // meaning "none"
+    gestureInProgress = false;
+  }
+  
+  void emulate() {
+    bool isTimeToEndClick = millis() > (timeOfClickStart + PULSE_DURATION);
+    if (isTimeToEndClick) endClick();
+  }
+} // namespace Button
+
+
+namespace Encoder {
+
+  enum state_t { 
+    ENC_STATE_IDLE = 0,  // Both encoder pins high
+    ENC_STATE_1 = 1,     // First encoder pin low, 2nd still high
+    ENC_STATE_2 = 2,     // Both encoder pins low
+    ENC_STATE_3 = 3      // First encoder pin high, 2nd still low
+  };
+
+  unsigned long stateTime = millis();
+  state_t state = ENC_STATE_IDLE;
+  int firstPin = 0;
+  int secondPin = 0;
+
+  bool rotationInProgress() {
+    return state != ENC_STATE_IDLE;
+  }
+
+  void dropFirstPin() {
+    pinMode(firstPin, OUTPUT);
+    digitalWrite(firstPin, LOW); // SHould already be low, but just to be safe. HIGH would be very bad because physical encoder, if used, could possibly short it to ground.
+    state = ENC_STATE_1;
+    stateTime = millis();
+  }
+
+  void dropSecondPin() {
+    pinMode(secondPin, OUTPUT);
+    digitalWrite(secondPin, LOW); // SHould already be low, but just to be safe. HIGH would be very bad because physical encoder, if used, could possibly short it to ground.
+    state = ENC_STATE_2;
+    stateTime = millis(); 
+  }
+
+  void raiseFirstPin() {
+    pinMode(firstPin, INPUT); // This is high Z on our side and there is a pull-up on the uSDX side
+    state = ENC_STATE_3;
+    stateTime = millis();   
+  }
+
+  void raiseSecondPin() {
+    pinMode(secondPin, INPUT); // This is high Z on our side and there is a pull-up on the uSDX side
+    state = ENC_STATE_IDLE;
+    stateTime = millis();   
+  }
+
+  void startRotation(int _firstPin, int _secondPin) {
+    firstPin = _firstPin;
+    secondPin = _secondPin;
+    dropFirstPin();  
+  }
+
+  void emulate() {
+    if (millis() - stateTime < PULSE_DURATION) return;
+    switch (state) {
+      case ENC_STATE_1: dropSecondPin(); break;
+      case ENC_STATE_2: raiseFirstPin(); break;
+      case ENC_STATE_3: raiseSecondPin(); gestureInProgress = false; break;
+    }
+  }
+} // namespace Encoder
+
+
 void performInputGesture(byte gesture) {
   gestureInProgress = true; 
   switch (gesture) {
-    case CLICK_LEFT_BUTTON: startButtonClick(LEFT_BUTTON_PIN); break;
-    case CLICK_RIGHT_BUTTON: startButtonClick(RIGHT_BUTTON_PIN); break;
-    case CLICK_ENCODER_BUTTON: startButtonClick(ENCODER_BUTTON_PIN); break;
-    case ROTATE_ENCODER_CLOCKWISE: startEncoderRotation(ROT_A_PIN, ROT_B_PIN); break;
-    case ROTATE_ENCODER_COUNTERCLOCKWISE: startEncoderRotation(ROT_B_PIN, ROT_A_PIN); break;
+    case CLICK_LEFT_BUTTON: Button::startClick(Pin::LEFT_BUTTON, HIGH); break;
+    case CLICK_RIGHT_BUTTON: Button::startClick(Pin::RIGHT_BUTTON, HIGH); break;
+    case CLICK_ENCODER_BUTTON: Button::startClick(Pin::ENCODER_BUTTON, HIGH); break;
+    case ROTATE_ENCODER_CLOCKWISE: Encoder::startRotation(Pin::ROT_A, Pin::ROT_B); break;
+    case ROTATE_ENCODER_COUNTERCLOCKWISE: Encoder::startRotation(Pin::ROT_B, Pin::ROT_A); break;
     case PUSH_TO_TALK_START: startPushToTalk(); break;
     case PUSH_TO_TALK_END: endPushToTalk(); break;
+    case RESET_THE_USDX: doUsdxReset(); break;
   }
 }
 
+void doUsdxReset() {
+  Button::startClick(Pin::USDX_RESET, LOW);
+}
+
 void startPushToTalk() {
-  pinMode(PUSH_TO_TALK_PIN, OUTPUT);
-  digitalWrite(PUSH_TO_TALK_PIN, LOW);
+  pinMode(Pin::PUSH_TO_TALK, OUTPUT);
+  digitalWrite(Pin::PUSH_TO_TALK, LOW);
   gestureInProgress = false;
 }
 
 void endPushToTalk() {
-  pinMode(PUSH_TO_TALK_PIN, INPUT);
+  pinMode(Pin::PUSH_TO_TALK, INPUT);
   gestureInProgress = false;
 }
 
-unsigned long timeOfClickStart = 0;
-int pinOfButtonClicked = 0;
-
-void startButtonClick(int buttonPin) {
-  pinMode(buttonPin, OUTPUT);
-  digitalWrite(buttonPin, HIGH);
-  timeOfClickStart = millis();
-  pinOfButtonClicked = buttonPin;
-}
-
-void endButtonClick() {
-  digitalWrite(pinOfButtonClicked, LOW);
-  pinMode(pinOfButtonClicked, INPUT);  // No PULLDOWN because there's already a pulldown on the uSDX side.
-  timeOfClickStart = 0; // meaning "none"
-  pinOfButtonClicked = 0; // meaning "none"
-  gestureInProgress = false;
-}
-
-enum encoder_state_t { 
-  ENC_STATE_IDLE = 0,  // Both encoder pins high
-  ENC_STATE_1 = 1,     // First encoder pin low, 2nd still high
-  ENC_STATE_2 = 2,     // Both encoder pins low
-  ENC_STATE_3 = 3      // First encoder pin high, 2nd still low
-};
-
-unsigned long encoderStateTime = millis();
-encoder_state_t encoderState = ENC_STATE_IDLE;
-int firstRotPin = 0;
-int secondRotPin = 0;
-
-void startEncoderRotation(int _firstRotPin, int _secondRotPin) {
-  firstRotPin = _firstRotPin;
-  secondRotPin = _secondRotPin;
-  dropFirstRotPin();  
-}
-
-void dropFirstRotPin() {
-  pinMode(firstRotPin, OUTPUT);
-  digitalWrite(firstRotPin, LOW); // SHould already be low, but just to be safe. HIGH would be very bad because physical encoder, if used, could possibly short it to ground.
-  encoderState = ENC_STATE_1;
-  encoderStateTime = millis();
-}
-
-void dropSecondRotPin() {
-  pinMode(secondRotPin, OUTPUT);
-  digitalWrite(secondRotPin, LOW); // SHould already be low, but just to be safe. HIGH would be very bad because physical encoder, if used, could possibly short it to ground.
-  encoderState = ENC_STATE_2;
-  encoderStateTime = millis(); 
-}
-
-void raiseFirstRotPin() {
-  pinMode(firstRotPin, INPUT); // This is high Z on our side and there is a pull-up on the uSDX side
-  encoderState = ENC_STATE_3;
-  encoderStateTime = millis();   
-}
-
-void raiseSecondRotPin() {
-  pinMode(secondRotPin, INPUT); // This is high Z on our side and there is a pull-up on the uSDX side
-  encoderState = ENC_STATE_IDLE;
-  encoderStateTime = millis();   
-}
-
-void emulateEncoder() {
-  if (millis() - encoderStateTime < BUTTON_CLICK_DURATION) return;
-  switch (encoderState) {
-    case ENC_STATE_1: dropSecondRotPin(); break;
-    case ENC_STATE_2: raiseFirstRotPin(); break;
-    case ENC_STATE_3: raiseSecondRotPin(); gestureInProgress = false; break;
-  }
-}
 
 void setup() {
   
   // These should remain INPUT at all times except when they are going to simulate a button press.
   // This protects them when hardware buttons are pushed, because there is no hardware protection on at least one of them.
   // Note that there's already a 10K pulldown on the uSDX side, so no point in doing so here.
-  pinMode(RIGHT_BUTTON_PIN,   INPUT);
-  pinMode(LEFT_BUTTON_PIN,    INPUT);
-  pinMode(ENCODER_BUTTON_PIN, INPUT);
-  pinMode(ROT_A_PIN,          INPUT);
-  pinMode(ROT_B_PIN,          INPUT);
+  pinMode(Pin::RIGHT_BUTTON,   INPUT);
+  pinMode(Pin::LEFT_BUTTON,    INPUT);
+  pinMode(Pin::ENCODER_BUTTON, INPUT);
+  pinMode(Pin::ROT_A,          INPUT);
+  pinMode(Pin::ROT_B,          INPUT);
 
-  pinMode(LCD_ENABLE_PIN,     INPUT_PULLUP  );
-  pinMode(LCD_POWER_PIN,      INPUT_PULLDOWN);
+  // This goes low when the LCD bits are ready to be read.
+  pinMode(Pin::LCD_ENABLE,     INPUT_PULLUP  );
+  
+  // This will be high when uSDX is powered on.
+  pinMode(Pin::LCD_POWER,      INPUT_PULLDOWN);
 
   // This is connected to an input pin on the uSDX, but we'll keep it hi-z except when pulling the uSDX pin low.
-  pinMode(PUSH_TO_TALK_PIN,   INPUT);
- 
-  Serial.begin(500000);
+  pinMode(Pin::PUSH_TO_TALK,   INPUT);
 
-  // LED will be used to indicate 1) waiting for power, and 2) buffer overflow.
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  // This is connected to the reset pin on the uSDX's 328. Pulse low to reset the 328.
+  pinMode(Pin::USDX_RESET,     OUTPUT);
+  digitalWrite(LED_BUILTIN,    LOW);
+
+  // LED will be used to indicate 0) startup/reset, 1) waiting for power, and 2) buffer overflow.
+  pinMode(LED_BUILTIN,         OUTPUT);
+  digitalWrite(LED_BUILTIN,    LOW);
+
 
   // Port D will be used to read the LCD interface   
   PORTD.DIRCLR = B00000000; // Set the entire port to INPUT
+
+  Serial.begin(500000);
 
   // Flashing LED indicates startup or reset.
   for (int i = 1; i < 10; i++) {
@@ -182,12 +221,12 @@ void loop() {
 
   // This waits for the uSDX to power up and then starts watching for lcd activity once the startup noise pass.
   digitalWrite(LED_BUILTIN, HIGH);
-  attachInterrupt(digitalPinToInterrupt(LCD_POWER_PIN), usdxPowerUpISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(Pin::LCD_POWER), usdxPowerUpISR, RISING);
   while (!usdxPowerOn);
-  detachInterrupt(digitalPinToInterrupt(LCD_POWER_PIN));
-  delay(500); // This skips noise from the startup.
-  attachInterrupt(digitalPinToInterrupt(LCD_ENABLE_PIN), lcdActivityISR, FALLING);
-  attachInterrupt(digitalPinToInterrupt(LCD_POWER_PIN), usdxPowerDownISR, FALLING);
+  detachInterrupt(digitalPinToInterrupt(Pin::LCD_POWER));
+  delay(100); // This skips noise from the startup.
+  attachInterrupt(digitalPinToInterrupt(Pin::LCD_ENABLE), lcdActivityISR, FALLING);
+  attachInterrupt(digitalPinToInterrupt(Pin::LCD_POWER), usdxPowerDownISR, FALLING);
   digitalWrite(LED_BUILTIN, LOW);
 
   // Will indicate power UP by sending RS=0 DATA=11111111 (i.e. Set DDRAM address to 127, which is invalid)
@@ -207,18 +246,18 @@ void loop() {
       performInputGesture(gesture);
     }
 
-    if (pinOfButtonClicked > 0) {
-      bool isTimeToEndClick = millis() > (timeOfClickStart + BUTTON_CLICK_DURATION);
-      if (isTimeToEndClick) endButtonClick();
+    if (Button::clickInProgress()) {
+      Button::emulate();
     }
 
-    if (encoderState != ENC_STATE_IDLE) {
-      emulateEncoder();
+    if (Encoder::rotationInProgress()) {
+      Encoder::emulate();
     }
   }  
 
   // uSDX power has been turned off, at this point.
-  detachInterrupt(digitalPinToInterrupt(LCD_POWER_PIN));
+  detachInterrupt(digitalPinToInterrupt(Pin::LCD_POWER));
+  detachInterrupt(digitalPinToInterrupt(Pin::LCD_ENABLE));
 
   // Will indicate power DOWN by sending RS=0 DATA=11111110 (i.e. Set DDRAM address to 126, which is invalid)
   Serial.write(markAsLcdByte(B00100111)); // This is RS=0 Nibble=1111
